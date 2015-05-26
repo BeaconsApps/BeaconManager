3b370fcd9cfecf75593bf339db30b6e86e21a18f
juan-ramirez <j127dra@gmail.com>
2015-05-25 22:53:44 -0500

    Beacon Manager first commit

---
+++ app/src/main/java/app/tohubee/beaconmanager/BeaconManagerApplication.java
@@ -0,0 +1,12 @@
+package app.tohubee.beaconmanager;
+
+import android.app.Application;
+
+/**
+ * Created by USER on 25/05/2015.
+ */
+public class BeaconManagerApplication extends Application {
+
+
+
+}
