07d396b4db6f2a40264ac77efa128fb8d04557df
juan-ramirez <j127dra@gmail.com>
2017-01-21 11:14:05 -0500

    Update 21/01/2017

--- app/src/main/java/app/tohubee/beaconmanager/BeaconManagerApplication.java
+++ app/src/main/java/app/tohubee/beaconmanager/BeaconManagerApplication.java
@@ -7,6 +7,10 @@ import android.app.Application;
  */
 public class BeaconManagerApplication extends Application {
 
+    @Override
+    public void onCreate() {
+        super.onCreate();
 
+    }
 
 }
