704e2a4613259b7ce8c6033b7ea95fd956c88e6d
juan-ramirez <j127dra@gmail.com>
2017-01-21 16:56:25 -0500

    App Id

--- app/src/main/java/app/tohubee/beaconmanager/BeaconManagerApplication.java
+++ app/src/main/java/app/tohubee/beaconmanager/BeaconManagerApplication.java
@@ -2,6 +2,8 @@ package app.tohubee.beaconmanager;
 
 import android.app.Application;
 
+import com.estimote.sdk.EstimoteSDK;
+
 /**
  * Created by USER on 25/05/2015.
  */
@@ -11,6 +13,13 @@ public class BeaconManagerApplication extends Application {
     public void onCreate() {
         super.onCreate();
 
+        String appId = "j127dra-gmail-com-s-your-o-nx7";
+        String appToken = "e0e6a97e22a6012d451c974cec091088";
+
+        //  App ID & App Token can be taken from App section of Estimote Cloud.
+        EstimoteSDK.initialize(this, appId, appToken);
+        // Optional, debug logging.
+        EstimoteSDK.enableDebugLogging(true);
     }
 
 }
